6fb83bcead4a42b9295cadda905ccf087cb8bc23
thangdtph27626 <109157942+thangdtph27626@users.noreply.github.com>
2023-02-28 21:58:13 +0700

    update noi dung bai viet

--- articles-project/src/main/java/com/articlesprojectTool/DBGenerator.java
+++ articles-project/src/main/java/com/articlesprojectTool/DBGenerator.java
@@ -81,7 +81,7 @@ public class DBGenerator implements CommandLineRunner {
 
         BaiViet baiViet = new BaiViet();
         baiViet.setTieuDe("Authorization on Angular Routes");
-        baiViet.setNoiDung("<H2>Giới thiệu\n</H2>");
+        baiViet.setNoiDung("<H2>Giới thiệu\n</H2></br><p>AngularJS đã đi một chặng đường dài kể từ khi được giới thiệu. Nó là một template JavaScript toàn diện cho Single Page Application (SPA) phát triển. Nó có một số tính năng tuyệt vời như 2 chiều ràng buộc, chỉ thị, vv và chủ đề này sẽ tập trung vào security ở trong Angular JS.</p><p>Để hiểu được bài này, bạn cần có kiến thức cơ bản về AngularJS, Angular Routing và Promises. Tôi sẽ hướng dẫn bạn từng bước để thực hiện security ở trong AngularJS</p>");
         baiViet.setTrangThai(1);
         baiViet.setCreateDate(1900800000L);
         baiViet.setThoiGianKiemDuyet(1900800000L);
@@ -92,7 +92,7 @@ public class DBGenerator implements CommandLineRunner {
 
         BaiViet baiViet1 = new BaiViet();
         baiViet1.setTieuDe("Abstract Class & Interface");
-        baiViet1.setNoiDung("<H2>Abstract Class</H2>");
+        baiViet1.setNoiDung("<H2>Abstract Class</H2></br><p>Được sử dụng cho hai mục đích:</p></br><p>1. Giảm thiểu lượng code lặp khi chúng ta có nhiều class khá giống nhau về lượng thuộc tính đặc tả và chức năng tương tác.</p><p></br><p>Ví dụ như trước đó chúng ta có các class mở rộng là Worker và Teacher cùng kế thừa lại các đặc tính của class Person. Trong trường hợp logic của chương trình không cần tạo object trực tiếp từ thao tác new Person() thì chúng ta có thể gắn từ khóa abstract vào phía trước từ khóa class trong định nghĩa của class Person.</p>");
         baiViet1.setTrangThai(1);
         baiViet1.setCreateDate(1900800000L);
         baiViet1.setThoiGianKiemDuyet(1900800000L);
@@ -103,7 +103,7 @@ public class DBGenerator implements CommandLineRunner {
 
         BaiViet baiViet2 = new BaiViet();
         baiViet2.setTieuDe("Types & Printer");
-        baiViet2.setNoiDung("<p>Tiếp tục câu chuyện sau khi đã copy/paste ");
+        baiViet2.setNoiDung("<p>Tiếp tục câu chuyện sau khi đã copy/paste và chạy được chương trình \"Hello World\" của C; Nói về cái từ khóa int được đặt ở phía trước khối main() thì mình cũng biết được lơ mơ về cái công dụng của nó. Cái này được gọi là type hinting - dịch nôm na là chỉ dẫn, định dạng kiểu dữ liệu - tức là kiểu dữ liệu sẽ xuất hiện ở vị trí nào đó trong code ví dụ như kiểu dữ liệu mà một biến sẽ lưu hoặc một hàm sẽ trả về khi được gọi.</p></br><p>Và những ngôn ngữ mà bắt buộc thao tác type hinting trong code thì thường được gọi là static typing, bởi vì kiểu của dữ liệu cần làm việc đã được cố định static chứ không được linh động dynamic như trường hợp của JavaScript nữa. Nếu chúng ta đặt một chuỗi vào vị trí đã được hint là một giá trị số học thì trình biên dịch compiler sẽ báo lỗi ngay khi thực hiện biên dịch sang tệp thực thi; Hoặc nếu sử dụng một trình thông dịch interpreter để chạy code trực tiếp thì khi gặp dữ liệu thực thi không đúng với hint cũng sẽ báo lỗi tức thì.</p> ");
         baiViet2.setTrangThai(1);
         baiViet2.setCreateDate(1900800000L);
         baiViet2.setThoiGianKiemDuyet(1900800000L);

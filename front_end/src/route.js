76967e3d1d636140b002e3732ce2424ecf841259
KHANHLINH03\MMC <linhntkph26829@fpt.edu.vn>
2023-03-02 14:34:48 +0700

    update form fe

--- front_end/src/route.js
+++ front_end/src/route.js
@@ -9,6 +9,26 @@ app.config(function ($routeProvider, $locationProvider) {
       templateUrl: "./pages/pages.html",
       // controller: ChiTietDonHangController,
     })
+    .when("/about", {
+      templateUrl: "./pages/about.html",
+      // controller: ChiTietDonHangController,
+    })
+    .when("/myblog", {
+      templateUrl: "./pages/myblog.html",
+      // controller: ChiTietDonHangController,
+    })
+    .when("/gioithieu", {
+      templateUrl: "./pages/gioithieu.html",
+      // controller: ChiTietDonHangController,
+    })
+    .when("/contact", {
+      templateUrl: "./pages/contact.html",
+      // controller: ChiTietDonHangController,
+    })
+    .when("/login", {
+      templateUrl: "./pages/login.html",
+      // controller: ChiTietDonHangController,
+    })
     .otherwise({
       redirectTo: "/trang-chu",
     });

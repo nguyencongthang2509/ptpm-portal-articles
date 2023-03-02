b21e126611741089011e44ea46b77998d6ff8183
Phạm Xuân Hải <haipxph26772@fpt.edu.vn>
2023-03-02 15:22:31 +0700

    "update form part 2"

--- front_end/src/controller/main.js
+++ front_end/src/controller/main.js
@@ -1,18 +1,19 @@
 app.controller("myCtrl", function ($scope) {
-  $scope.$on("$includeContentLoaded", function () {
-    document
-      .querySelector(".toggle-sidebar-btn")
-      .addEventListener("click", function () {
-        document.querySelector("body").classList.toggle("toggle-sidebar");
-      });
-    document
-      .querySelector(".search-bar-toggle")
-      .addEventListener("click", function () {
-        document
-          .querySelector(".search-bar")
-          .classList.toggle("search-bar-show");
-      });
-  });
-
-  
+  window.onload = function () {
+    allHaha.allHihi();
+    $scope.$on("$includeContentLoaded", function () {
+      document
+        .querySelector(".toggle-sidebar-btn")
+        .addEventListener("click", function () {
+          document.querySelector("body").classList.toggle("toggle-sidebar");
+        });
+      document
+        .querySelector(".search-bar-toggle")
+        .addEventListener("click", function () {
+          document
+            .querySelector(".search-bar")
+            .classList.toggle("search-bar-show");
+        });
+    });
+  };
 });

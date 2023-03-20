df7403bfe11c4f0e3263948c6e65ccf8b1ac61f2
thangdtph27626 <109157942+thangdtph27626@users.noreply.github.com>
2023-03-20 17:53:27 +0700

    create comment

--- front_end/src/controller/detailArticleController.js
+++ front_end/src/controller/detailArticleController.js
@@ -1,49 +1,55 @@
 window.detailArticleCtrl = function (
-  $scope,
-  $http,
-  $rootScope,
-  $routeParams,
-  ArticleService,
-  CommentService,
-  CategoryService,
-  MyArticleService
+    $scope,
+    $http,
+    $rootScope,
+    $routeParams,
+    ArticleService,
+    CommentService,
+    CategoryService,
+    env,
+    MyArticleService
 ) {
   $scope.comment = {
     articlesId: $routeParams.id,
     content: "",
   };
 
-  $scope.replyComment = {
+  $scope.replyCommentUser = {
     articlesId: $routeParams.id,
     content: "",
     reply: "",
   };
 
-  var id = $routeParams.id;
-
-  MyArticleService.fetchMyArticleById(id).then(function () {
+  MyArticleService.fetchMyArticleById($routeParams.id).then(function () {
     $scope.myArticleById = MyArticleService.getMyArticleById();
-    console.log($scope.myArticleById);
   });
 
   CommentService.fetchComments($routeParams.id).then(function(){
     $scope.comments = CommentService.getListCommentByArticle();
-    console.log( $scope.comments)
   });
 
   $scope.deleteMyArticle = function () {
-    toastr.warning("Bạn có chắc muốn xóa?");
     if (confirm("Bạn có chắc muốn xóa?")) {
       $http.delete(myArticleAPI + "/delete-article/" + id).then(
-        function (response) {
-          toastr.success("Xóa thành công");
-          console.log("Thành công rồi haha");
-        },
-        function (error) {
-          toastr.error("Có lỗi xảy ra");
-          console.log(error);
-          console.log("Thất bại rồi xem lại code đi");
-        }
+          function (response) {
+            toastr.success("Xóa thành công", "Thông báo!", {
+              timeOut: 5000,
+              closeButton: true,
+              progressBar: true,
+              positionClass: "toast-top-center",
+            });
+            console.log("Thành công rồi haha");
+          },
+          function (error) {
+            toastr.error("Có lỗi xảy ra", "Thông báo!", {
+              timeOut: 5000,
+              closeButton: true,
+              progressBar: true,
+              positionClass: "toast-top-center",
+            });
+            console.log(error);
+            console.log("Thất bại rồi xem lại code đi");
+          }
       );
     } else {
       toastr.info("Đã hủy xóa");
@@ -51,14 +57,33 @@ window.detailArticleCtrl = function (
   };
 
   $scope.replyComment = function(id, userName){
+    [...document.querySelectorAll(".fromReplyComment")].map(item =>{
+      item.style.display = "none"
+    })
     var reply = document.getElementById(id)
-    console.log(id)
     reply.style.display = "block"
-    $scope.replyComment.reply = id
-    $scope.replyComment.content = "@" +userName + " "
+    $scope.replyCommentUser.reply = id
+    $scope.replyCommentUser.content = "@" +userName + " "
   }
   $scope.closeForm = function(id){
     var reply = document.getElementById(id)
     reply.style.display = "none"
+  }
+
+  $scope.createComment = function(){
+    $http.post(env.API_URL+"/comment/create", $scope.comment ).then(function(response){
+      $scope.comment = {
+        articlesId: $routeParams.id,
+        content: "",
+      };    })
+  }
+  $scope.CreateReplyComment = function(){
+    $http.post(env.API_URL+"/comment/create", $scope.replyCommentUser ).then(function(response){
+      $scope.replyCommentUser = {
+        articlesId: $routeParams.id,
+        content: "",
+        reply: "",
+      };
+    })
   }
 };

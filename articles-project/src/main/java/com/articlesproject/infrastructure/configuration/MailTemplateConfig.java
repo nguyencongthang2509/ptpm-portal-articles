20b58c36828d1bb01427e0eae026f634a09bce91
thangdtph27626 <109157942+thangdtph27626@users.noreply.github.com>
2023-03-24 11:59:39 +0700

    send gmail

---
+++ articles-project/src/main/java/com/articlesproject/infrastructure/configuration/MailTemplateConfig.java
@@ -0,0 +1,24 @@
+package com.articlesproject.infrastructure.configuration;
+
+import com.articlesproject.infrastructure.constant.MailConstant;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class MailTemplateConfig {
+    @Bean
+    public ITemplateResolver thymeleafClassLoaderTemplateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix(MailConstant.TEMPLATE_PREFIX);
+        templateResolver.setSuffix(MailConstant.TEMPLATE_SUFFIX);
+        templateResolver.setTemplateMode(MailConstant.TEMPLATE_MODE);
+        templateResolver.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+        templateResolver.setOrder(1);
+        templateResolver.setCheckExistence(true);
+        return templateResolver;
+    }
+}
---
+++ articles-project/src/main/java/com/articlesproject/infrastructure/constant/MailConstant.java
@@ -0,0 +1,15 @@
+package com.articlesproject.infrastructure.constant;
+
+public class MailConstant {
+    public static final String TEMPLATE_PREFIX = "mail-templates/";
+
+    public static final String TEMPLATE_SUFFIX = ".html";
+
+    public static final String TEMPLATE_MODE = "HTML";
+
+    public static final String TEMPLATE_MAIL = "email";
+
+    public static final String LOGO_PATH = "mail-templates/logo.png";
+
+}
+
